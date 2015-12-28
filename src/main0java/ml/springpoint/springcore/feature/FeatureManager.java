f835a27c4a8d15afa7e046f5e30b4737f65204df
SirFaizdat <sirfaizdat@springpoint.ml>
2015-12-28 00:51:04 -0500

    Add copyright notice

---
+++ src/main0java/ml/springpoint/springcore/feature/FeatureManager.java
@@ -0,0 +1,85 @@
+/*
+ *  Copyright (C) 2015 Springpoint Software and Contributors
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ml.springpoint.springcore.feature;
+
+import ml.springpoint.springcore.SpringPlugin;
+import ml.springpoint.springcore.module.ModuleFeature;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Responsible for enabling features if they are chosen by the developer.
+ * All features are registered here.
+ * <p/>
+ * <table>
+ * <tr>
+ * <th>Feature</th>
+ * <th>Description</th>
+ * </tr>
+ * <tr>
+ * <td>Modules</td>
+ * <td>Split up your plugin into sub-parts. Modules are like SpringPlugin classes,
+ * but can be enabled/disabled independently of their parent plugin and have
+ * a seperate data folder for their seperate configuration files.</td>
+ * </tr>
+ * </table>
+ *
+ * @author SirFaizdat
+ * @since 1.0
+ */
+public class FeatureManager {
+
+    SpringPlugin plugin;
+    Map<String, Feature> featureMap;
+
+    public FeatureManager(SpringPlugin plugin) {
+        this.plugin = plugin;
+        featureMap = new HashMap<>();
+        featureMap.put("modules", new ModuleFeature(plugin));
+    }
+
+    /**
+     * Initialize a feature so that it is ready for use.
+     *
+     * @param name The name of the feature to use. If the feature does not
+     *             exist or the name is incorrect, this will throw a NullPointerException.
+     */
+    public void use(String name) {
+        featureMap.get(name).init();
+    }
+
+    /**
+     * De-initialize all of the features.
+     */
+    public void disable() {
+        for (Feature f : featureMap.values()) f.deinit();
+    }
+
+    /**
+     * Gets a feature by name.
+     *
+     * @param name The name of the feature.
+     * @return The Feature object. From here, you would cast it to the appropriate object.
+     * For example, if you called <code>get("modules");</code>, you would cast it to {@link ModuleFeature}.
+     */
+    public Feature get(String name) {
+        return featureMap.get(name);
+    }
+
+}
